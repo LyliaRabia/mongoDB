9b32b243f565aaf131b2cff8d923a6ec37eff500
Amine <aminesaker@outlook.com>
2018-03-14 17:20:31 +0100

    mise a jour appMongo

--- src/main/java/mongo/Main/AppMongo.java
+++ src/main/java/mongo/Main/AppMongo.java
@@ -2,6 +2,7 @@ package mongo.Main;
 
 import com.mongodb.MongoClient;
 import mongo.Domain.Adress;
+import mongo.Domain.Article;
 import mongo.Domain.Person;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -17,21 +18,46 @@ public class AppMongo
         Morphia morphia = new Morphia();
         MongoClient mongo = new MongoClient();
         morphia.map(Person.class).map(Adress.class);
+        morphia.map(Article.class);
         Datastore ds = morphia.createDatastore(mongo, "my_database");
 
-        Person p = new Person();
-        p.setName("Tintin");
+        //Articles
+        Article article1 = new Article("kinder", 25);
+        Article article2 = new Article("ferero", 30);
 
-        Adress address = new Adress();
-        address.setStreet("123 Some street");
-        address.setCity("Some city");
-        address.setPostCode("123 456");
-        address.setCountry("Some country");
-        //set address
-        p.addAddress(address);
-        // Save the POJO
-       // ds.save(p);
+        // Personnes
+        Person person = new Person("titi");
+        Person person1 = new Person("toto");
+        Person person2 = new Person("tata");
+
+        //Adresses
+        Adress address = new Adress("34 rue guy ropartz","35700","Rennes", "France" );
+        Adress address1 = new Adress("20 rue de leon","35000","Rennes", "France" );
+        Adress address2 = new Adress("30 kitchen street","65420","kitchen city", "House" );
+
+
+        person.addAddress(address);
+        person.addAddress(address1);
+        person.addAddress(address2);
+
+
+        article1.addPerson(person);
+        article1.addPerson(person1);
+        article1.addPerson(person2);
+
+
+        ds.save(article1);
+        ds.save(article2);
+        ds.save(person);
+        ds.save(person1);
+        ds.save(person2);
+
         for (Person e : ds.find(Person.class))
             System.out.println(e.toString());
+
+        for (Article e : ds.find(Article.class))
+            System.out.println(e.toString());
+
+
     }
 }
